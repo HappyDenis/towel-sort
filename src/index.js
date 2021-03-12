342c0a75617a83d3828d0b49f4694ed1cacb99c4
happydenis <dao.denis@gmail.com>
2021-03-12 13:37:12 +0300

    feat: resolve added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if  (typeof(matrix) == 'undefined') return ([]);
+  if  (matrix.length == 0) return ([]);
+
+  var matrix2 = [];
+  if (matrix == 'undefined') {return (matrix = [])}
+for (var i = 0; i < matrix.length; i++) {
+  if (i % 2 == 0) { 
+    matrix2 += matrix[i].join(' ');}
+  if (i % 2 > 0) { 
+    matrix2 += matrix[i].reverse().join(' ');}
+
+  if (matrix.length-1 > i > 0) { matrix2 += ' ';}
+  //console.log((matrix2));
+  }
+  return (matrix2.split(' '));
 }
